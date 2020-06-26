e54087534b03d8cef7ffb672058a54a64bbbc205
Nazare <nazarehturmina@gmail.com>
2020-06-26 18:46:36 +0800

    Add Clarifi to the back end instead of front end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,15 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'ef1582b0063d4b87a1e2e624f79c61e9'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json('unable to work with API'));
+}
+
 const handleImage = (req, res, db) => {
     db('users')
         .where('id', '=', req.body.id)
@@ -8,5 +20,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage ,
+    handleApiCall: handleApiCall
 }
